3f7059e04eba438f4a66f40e250cd28f5919e3df
XiaoRanLiu3119 <592923276@qq.com>
2023-08-09 17:49:53 +0800

    去除首页豆瓣热播标识

--- app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
+++ app/src/main/java/com/github/tvbox/osc/ui/adapter/HomeHotVodAdapter.java
@@ -32,9 +32,7 @@ public class HomeHotVodAdapter extends BaseQuickAdapter<Movie.Video, BaseViewHol
         TextView tvRate = helper.getView(R.id.tvRate);
         if (Hawk.get(HawkConfig.HOME_REC, 0) == 2){
             tvRate.setText(ApiConfig.get().getSource(item.sourceKey).getName());
-        }else if(Hawk.get(HawkConfig.HOME_REC, 0) == 0){
-            tvRate.setText("豆瓣热播");
-        }else {
+        } else {
             tvRate.setVisibility(View.GONE);
         }
 
